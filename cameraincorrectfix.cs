7d356851e3692ead3f047ca9fe33e41161c93e16
Razvan201013 <72081388+Razvan201013@users.noreply.github.com>
2022-08-23 16:23:06 +0300

    Add files via upload

---
+++ cameraincorrectfix.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class cameraincorrectfix : MonoBehaviour
+{
+    public GameObject cam;
+
+    private void Awake()
+    {
+        if (GetComponent<PhotonView>().IsMine)
+        {
+            cam.SetActive(true);
+        }
+        else
+        {
+            print("Incorrect PhotonView");
+        }
+    }
+
+}
